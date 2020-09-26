9ba736325df9f8c0c0747bb1ed452bac14a0a3f9
maciej <mrostanski@gmail.com>
2020-09-26 12:15:22 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # terraform-aws-account-info
-My awesome repository managed by Terraform
+
+Simple module for demonstrating terraform source using GitHub
+
+## String outputs
+
+* account_alias
+* account_id
+* account_url
+
+## Map outputs
+
+* aws
+  * url
+  * id
+  * alias
---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "account_alias" {
+  value = data.aws_iam_account_alias.current.account_alias
+}
+
+output "account_url" {
+  value = "https://${data.aws_iam_account_alias.current.account_alias}.signin.aws.amazon.com/console"
+}
+
+# Przykład konstrukcji mapu jako outputu
+
+output "account" {
+  value = {
+    id    = data.aws_caller_identity.current.account_id
+    alias = data.aws_iam_account_alias.current.account_alias
+    url   = "https://${data.aws_iam_account_alias.current.account_alias}.signin.aws.amazon.com/console"
+  }
+}
